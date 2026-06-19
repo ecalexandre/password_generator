4340c362a64174f46014b5ca77201ae2a225ea65
Elliot C. Alexandre <elliot.c.alexandre@proton.me>
2026-06-19 16:47:49 -0400

    Added 'numbers or no numbers' options

--- password_generator.py
+++ password_generator.py
@@ -1,38 +1,65 @@
 import random
-def strong_password_generator(get_value_entry, symbol_option, display_place_entry) -> None:
+def strong_password_generator(get_value_entry, symbol_option, number_option, display_place_entry) -> None:
     """
     Displays a chosen amount of random characters for a strong password in the window
 
     """
-    characters : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
+    characters_without_numbers : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
                                    'o', 'p', 'q' 'r' 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                                    '#','$','%','*','(',')','@','!']
 
-    characters_without_symbols : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
+    characters_without_symbols_and_numbers : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
                                    'o', 'p', 'q' 'r' 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
-    numbers : list[str] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    characters_without_symbols : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
+                                   'o', 'p', 'q' 'r' 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     
+    characters_with_numbers_and_symbols : list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
+                                   'o', 'p', 'q' 'r' 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                   '#','$','%','*','(',')','@','!', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
     amount_of_characters = get_value_entry.get()
     
     val : int = int(amount_of_characters)
-
-    match symbol_option.get():
+    
+    #Results for each options
+    match symbol_option.get(), number_option.get():
     	
-    	case 1:
-    	  random_characters : random = random.choices(characters, k=val)
+    	case 1,1:
+    	  random_characters_with_numbers_and_symbols : random = random.choices(characters_with_numbers_and_symbols, k=val)
     	  
-    	  random_password : join = "".join(random_characters)
+    	  random_password_with_numbers_and_symbols : join = "".join(random_characters_with_numbers_and_symbols)
     	  
     	  display_place_entry.delete(0,END)
     	  
-    	  display_place_entry.insert(0, random_password)
+    	  display_place_entry.insert(0,random_password_with_numbers_and_symbols)
     	
-    	case 2:
-          random_characters_without_symbols : random = random.choices(characters_without_symbols, k=val)
-          random_password_without_symbols : join = "".join(random_characters_without_symbols)
+    	case 1,2:
+          random_characters_without_numbers : random = random.choices(characters_without_numbers, k=val)
+          
+          random_password_without_numbers : join = "".join(random_characters_without_numbers)
+          
           display_place_entry.delete(0,END)
+          
+          display_place_entry.insert(0, random_password_without_numbers)
+    	
+    	case 2,1:
+          random_characters_without_symbols : random = random.choices(characters_without_symbols, k=val)
+          
+          random_password_without_symbols : join = "".join(random_characters_without_symbols)
+          
+          display_place_entry.delete(0,END)
+          
           display_place_entry.insert(0, random_password_without_symbols)
+    	
+    	case 2,2:
+          random_characters_without_symbols_and_numbers : random = random.choices(characters_without_symbols_and_numbers, k=val)
+          
+          random_password_without_symbols_and_numbers : join = "".join(random_characters_without_symbols_and_numbers)
+          
+          display_place_entry.delete(0,END)
+          
+          display_place_entry.insert(0, random_password_without_symbols_and_numbers)    
 
 
 import tkinter as tk; from tkinter import *
@@ -49,7 +76,7 @@ window.configure(bg='black')
 window.title('Password generator app 🔒')
 
 #window's dimensions
-window.geometry('600x300')
+window.geometry('1000x600')
 
 #app icon image
 app_icon : PhotoImage = tk.PhotoImage(file='C:\\Users\\ecalexandre\\Downloads\\padlorfe.png')
@@ -63,10 +90,13 @@ text : Label = tk.Label(window,
 	             text='Password generator 🔒',
 	             bg='black',
 	             fg='#8803fc',
-	             font=('Arial', 30, 'bold'),
+	             font=('Arial', 15, 'bold'),
 	             image=photo,
 	             compound='bottom')
 text.grid(row=3, column=2)
+
+
+
 
 
 #symbols/no symbols text
@@ -77,32 +107,73 @@ symbols_label : Label = tk.Label(window,
 	                             fg='#fc035a')
 symbols_label.grid(row=0,column=0)
 
-''' 'Symbols/No symbols' options'''
-Symbol_or_no_Symbols = IntVar()
+#Symbols/No symbols options
+symbol_or_no_Symbols = IntVar()
 
 #yes option
-yes_option = Radiobutton(window,
-	                  text='Yes 👍',
-	                  variable=Symbol_or_no_Symbols,
-	                  value=1,
-	                  fg='#1c7a21',
-	                  bg='black',
-	                  font=('Arial', 15, 'bold'),
-	                  activebackground='black',
-	                  activeforeground='#1c7a21')
-yes_option.grid(row=1,column=0)
+yes_option_symbols : Radiobutton = Radiobutton(window,
+	                                           text='Yes 👍',
+	                                           variable=symbol_or_no_Symbols,
+	                                           value=1,
+	                                           fg='#1c7a21',
+	                                           bg='black',
+	                                           font=('Arial', 15, 'bold'),
+	                                           activebackground='black',
+	                                           activeforeground='#1c7a21')
+yes_option_symbols.grid(row=1,column=0)
 
 #no option
-no_option = Radiobutton(window,
-	                    text='No 👎',
-	                    variable=Symbol_or_no_Symbols,
-	                    value=2,
-	                    fg='#fc3503',
-	                    bg='black',
-	                    font=('Arial', 15, 'bold'),
-	                    activebackground='black',
-	                    activeforeground='#fc3503')
-no_option.grid(row=2,column=0)
+no_option_symbols : Radiobutton = Radiobutton(window,
+	                                          text='No 👎',
+	                                          variable=symbol_or_no_Symbols,
+	                                          value=2,
+	                                          fg='#fc3503',
+	                                          bg='black',
+	                                          font=('Arial', 15, 'bold'),
+	                                          activebackground='black',
+	                                          activeforeground='#fc3503')
+no_option_symbols.grid(row=2,column=0)
+
+
+
+
+#Numbers options text
+numbers_label : Label = tk.Label(window,
+	                            text='Do you want numbers in your password or not',
+	                            font=('Arial', 15, 'bold'),
+	                            bg='black',
+	                            fg='#fc035a')
+numbers_label.grid(row=3,column=0)
+
+#Numbers options
+numbers_or_no_numbers = IntVar()
+
+#yes option
+yes_option_numbers : Radiobutton = tk.Radiobutton(window,
+	                                            text='Yes 👍',
+	                                            variable=numbers_or_no_numbers,
+	                                            value=1,
+	                                            fg='#1c7a21',
+	                                            bg='black',
+	                                            font=('Arial', 15, 'bold'),
+	                                            activebackground='black',
+	                                            activeforeground='#1c7a21')
+yes_option_numbers.grid(row=4, column=0)
+
+#no option
+no_option_numbers : Radiobutton = tk.Radiobutton(window,
+	                                            text='No 👎',
+	                                            variable=numbers_or_no_numbers,
+	                                            value=2,
+	                                            fg='#fc3503',
+	                                            bg='black',
+	                                            font=('Arial', 15, 'bold'),
+	                                            activebackground='black',
+	                                            activeforeground='#fc3503')
+no_option_numbers.grid(row=5, column=0)
+
+
+
 
 
 #'Amount of characters' text
@@ -111,12 +182,12 @@ label : Label = tk.Label(window,
 	             font=('Arial', 11, 'bold'),
 	             bg='black',
 	             fg='#2060a1')
-label.grid(row=4, column=0)
+label.grid(row=6, column=0)
 
 #amount of characters's entry
 character_amount_entry : entry = Entry(window,
 	                                   font=('Arial', 50))
-character_amount_entry.grid(row=5,column=0)
+character_amount_entry.grid(row=7,column=0)
 
 
 
@@ -127,22 +198,26 @@ def enter_pressed(event) -> None:
     """
 
     character_amount_entry.get()
-    strong_password_generator(character_amount_entry, Symbol_or_no_Symbols, password_displayed_entry)
+    strong_password_generator(character_amount_entry, symbol_or_no_Symbols, numbers_or_no_numbers, password_displayed_entry)
 character_amount_entry.bind('<Return>', enter_pressed) #checks if user pressed Enter
 
 
 #'Password' text
 password_label : Label = tk.Label(window,
 	                      text='Password 🔐:',
-	                      font=('Courier', 20),
+	                      font=('Courier', 15),
 	                      bg='black',
 	                      fg='#699e24')
-password_label.grid(row=6,column=0)
+password_label.grid(row=8,column=0)
+
+
 
 #Password displayed entry
 password_displayed_entry : Entry = Entry(window,
 	                                    font=('Arial', 50),)
-password_displayed_entry.grid(row=7,column=0)
+password_displayed_entry.grid(row=9,column=0)
+
+
 
 #copy password function for button
 def copy_text() -> None:
@@ -160,6 +235,6 @@ copy_password_button : Button = tk.Button(window,
 	                                      activebackground='black',
 	                                      activeforeground='#f2e124',
 	                                      command=copy_text)
-copy_password_button.grid(row=7, column=1)	                                         
+copy_password_button.grid(row=9, column=1)	                                         
 
 window.mainloop()
